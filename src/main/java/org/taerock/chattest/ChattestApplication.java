0fc0c31b01e3478a29852ae2e50077de1c8dcb24
LTH4823 <peter4823zx@naver.com>
2022-07-06 21:20:45 +0900

    Initial commit

---
+++ src/main/java/org/taerock/chattest/ChattestApplication.java
@@ -0,0 +1,13 @@
+package org.taerock.chattest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChattestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChattestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/taerock/chattest/ChattestApplicationTests.java
@@ -0,0 +1,13 @@
+package org.taerock.chattest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChattestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
